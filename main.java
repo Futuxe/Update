cf73c02cc5455cb9db287aede34fbac3f28aa095
RJ <103357673+Futuxe@users.noreply.github.com>
2025-02-05 21:33:57 +0100

    Update main.java

--- main.java
+++ main.java
@@ -3,3 +3,4 @@ when goes to fox
 goto love jbshaksb
 javjksnxb
 java.update.script
+https://store.supercell.com/plmore more more more more more more more https://github.com/futuxehttps://github.com/futuxehttps://github.com/futuxehttps://github.com/futuxehttps://github.com/futuxehttps://github.com/futuxe
